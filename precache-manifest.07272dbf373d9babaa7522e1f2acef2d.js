5a3d5e0d23c18e54363c229096268119dd17a4e9
Max <mctestpub@gmail.com>
2024-09-02 18:25:30 +0300

    Updates

---
+++ precache-manifest.07272dbf373d9babaa7522e1f2acef2d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cfcec6e9e2c3fedfedfd033c8059781",
+    "url": "/cyp-inv-calc/index.html"
+  },
+  {
+    "revision": "a44471e65d7caed088dc",
+    "url": "/cyp-inv-calc/static/css/main.622db4f5.chunk.css"
+  },
+  {
+    "revision": "1582e325d1bcf091e744",
+    "url": "/cyp-inv-calc/static/js/2.ca150098.chunk.js"
+  },
+  {
+    "revision": "2e7b5277f70c872aecfe",
+    "url": "/cyp-inv-calc/static/js/3.940f5f6f.chunk.js"
+  },
+  {
+    "revision": "a44471e65d7caed088dc",
+    "url": "/cyp-inv-calc/static/js/main.f8212517.chunk.js"
+  },
+  {
+    "revision": "ed9fddc142ce9feb93ef",
+    "url": "/cyp-inv-calc/static/js/runtime~main.e14e88f7.js"
+  }
+]);
